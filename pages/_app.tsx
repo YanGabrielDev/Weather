7cb05c58636d731b516f2ceed5d6512279076963
Yan <yangabriel2012@gmail.com>
2023-09-11 22:37:07 -0300

    feat: instalando styled-compoents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { GlobalStyles } from "../styles/globals-styles";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "../styles/theme/darkTheme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
