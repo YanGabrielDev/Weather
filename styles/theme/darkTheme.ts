8135ad9cb343ff49473239b3ee6b270184fb5609
Yan <yangabriel2012@gmail.com>
2023-09-11 22:36:57 -0300

    feat: preparando darkmode

---
+++ styles/theme/darkTheme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    colors:{
+        main: "#101112",
+        text: "#fff",
+        secondaryMain: "#202224",
+        primary: "#7dd7ff"
+    }
+}
---
+++ styles/theme/lightTheme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    colors:{
+        main: "#fff",
+        text: "#1e1e1f",
+        secondaryMain: "#d4d4d4",
+        primary: "#0062ff"
+    }
+}
