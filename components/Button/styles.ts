3abe0e9563ddd2e08a1c49ca62ac01a59bb2f812
Yan <yangabriel2012@gmail.com>
2023-09-14 00:37:38 -0300

    styles: estilizando button

--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -1,34 +1,52 @@
 import { styled, css, DefaultTheme } from "styled-components";
+import { ButtonProps } from ".";
 type KeysThemeProp = {
-  [key: string]: (theme: DefaultTheme) => ReturnType<typeof css>;
+  [key: string]: (theme?: DefaultTheme) => ReturnType<typeof css>;
 };
 
-type ContainerProps = {
-  color?: "pramaty" | "secondaty";
-  fontSize?: "small" | "medium" | "large";
-};
 
 const colors: KeysThemeProp = {
-  primary: (theme: DefaultTheme) => css`
-    background-color: ${theme.colors.primary};
+  primary: (theme?: DefaultTheme) => css`
+    background-color: ${theme?.colors.primary};
   `,
 };
 
 const fontSizes: KeysThemeProp = {
   small: (theme) => css`
-    font-size: ${theme.font?.sizes.small};
+    font-size: ${theme?.font?.sizes.small};
   `,
   medium: (theme) => css`
-    font-size: ${theme.font?.sizes.medium};
+    font-size: ${theme?.font?.sizes.medium};
   `,
   large: (theme) => css`
-    font-size: ${theme.font?.sizes.large};
+    font-size: ${theme?.font?.sizes.large};
   `,
 };
 
-export const Container = styled.button<ContainerProps>`
-  ${({ theme, color = "primary", fontSize = "medium" }) => css`
-    ${colors[color](theme)};
+const buttonRounded: KeysThemeProp = {
+  full: () => css`
+    border-radius: 50%;
+  `,
+  medium: () => css`
+    font-size: "0.25rem";
+  `,
+};
+
+export const Container = styled.button<ButtonProps>`
+  ${({ theme, color = "primary", fontSize = "medium", rounded = "medium" }) => css`
+   border: none ;
+   color: ${theme.colors.text};
+   cursor: pointer;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+  ${colors[color](theme)};
     ${fontSizes[fontSize](theme)};
+    ${buttonRounded[rounded]()};
+
+    svg{
+        width: 22px;
+        height: 30px;
+    }
   `}
 `;
