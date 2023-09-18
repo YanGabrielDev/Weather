c87f6083d4b092bf52b5c065b27e3ebbdf3f711a
Yan <yangabriel2012@gmail.com>
2023-09-18 01:06:49 -0300

    feat: adicionando as cores novas

--- styles/theme/darkTheme.ts
+++ styles/theme/darkTheme.ts
@@ -1,10 +1,30 @@
 import { DefaultTheme } from "styled-components";
+import { Poppins } from "@next/font/google";
+
+export const poppins = Poppins({
+    weight: "500",
+    style: "normal",
+    subsets: ["latin"]
+
+})
 
 export const darkTheme: DefaultTheme = {
     colors:{
         main: "#101112",
+        background: "#101112",
         text: "#fff",
         secondaryMain: "#202224",
-        primary: "#7dd7ff"
+        primary: "#7dd7ff",
+        gray: "#b5b5b5ab"
+    },
+    font:{
+        family:{
+            poppins: poppins.style.fontFamily
+        },
+        sizes: {
+            small: "0.875rem",
+            medium: "1rem",
+            large: "1.5rem"
+        }
     }
 }
--- styles/theme/lightTheme.ts
+++ styles/theme/lightTheme.ts
@@ -1,10 +1,28 @@
 import { DefaultTheme } from "styled-components";
+import { Poppins } from "@next/font/google";
 
+export const poppins = Poppins({
+    weight: "500",
+    style: "normal",
+    subsets: ["latin"]
+})
 export const lightTheme: DefaultTheme = {
     colors:{
         main: "#fff",
+        background: "#f2f3f4",
         text: "#1e1e1f",
         secondaryMain: "#d4d4d4",
-        primary: "#5193fd"
+        primary: "#5193fd",
+        gray: "#b1b0b0fa"
+    },
+    font:{
+        family:{
+            poppins: poppins.style.fontFamily
+        },
+        sizes: {
+            small: "0.875rem",
+            medium: "1rem",
+            large: "1.5rem"
+        }
     }
 }
