ffc2b02359bbfa6543e7281fa68d51f4e15caec2
Yan <yangabriel2012@gmail.com>
2023-09-19 00:12:24 -0300

    feat: criando o component layout

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,11 +1,26 @@
-import { Header } from "../Header"
+import { Moon, Sun } from "lucide-react";
+import { Button, Container, Header, Location, TextField } from "../";
+import { ReactNode } from "react";
 
-export const Layout = () => {
-    return(
-        <>
-        <Header>
-            
-        </Header>
-        </>
-    )
+interface LayoutProps{
+    darkMode: boolean,
+    handleChangeTheme: () => void,
+    children: ReactNode
 }
+
+export const Layout = ({darkMode, handleChangeTheme, children}: LayoutProps) => {
+  return (
+    <>
+      <Header>
+        <Location city="Belo Horizonte" country="BR"/>
+        <TextField placeholder="Procure a cidade..." />
+        <Button rounded="full" color="primary" onClick={handleChangeTheme}>
+          {darkMode ? <Moon /> : <Sun />}
+        </Button>
+      </Header>
+      <Container>
+        {children}
+      </Container>
+    </>
+  );
+};
