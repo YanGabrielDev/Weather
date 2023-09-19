14c1b541e3295ec9deb43bf302c49e7cb56c688f
Yan <yangabriel2012@gmail.com>
2023-09-19 00:12:08 -0300

    style: adiciona space between ao header

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -2,10 +2,9 @@ import { styled,css } from 'styled-components'
 
 export const Container = styled.header`
 ${({theme}) => css`
-background-color: ${theme.colors.main};
 display: flex;
 align-items: center;
 padding: 1rem;
-
+justify-content: space-between;
 `}
 `
