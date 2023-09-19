833f8343e1c1bc63e46e7231eadffc800b301301
Yan <yangabriel2012@gmail.com>
2023-09-19 00:11:24 -0300

    fix: exportando os components do index

---
+++ components/index.tsx
@@ -0,0 +1,8 @@
+import { Button } from "./Button";
+import { Container } from "./Container";
+import { Header } from "./Header";
+import { Layout } from "./Layout";
+import { Location } from "./Location";
+import { TextField } from "./TextField";
+
+export { Button, Container, Header, Layout, Location, TextField };
