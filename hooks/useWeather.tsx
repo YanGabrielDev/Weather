312d684cda203cc5e27c40d1bb04983dc4d2944b
Yan <yangabriel2012@gmail.com>
2023-09-19 23:16:17 -0300

    fix: refatorando useWeather

--- hooks/useWeather.tsx
+++ hooks/useWeather.tsx
@@ -1,21 +1,28 @@
 import { restApiProvider } from "../services/restApiService";
 import { useEffect, useState } from "react";
-export const useWeather = () => {
-  const [data, setdata] = useState();
-  const loadWeather = async () => {
-    
-    const position =  await new Promise<GeolocationPosition>( (resolve, reject) =>{
-      navigator.geolocation.getCurrentPosition(resolve, reject); 
-    }
-    )
-    const lat = position.coords.latitude
-    const long = position.coords.longitude
+import { WeatherResponse } from "../interfaces/Weather.interface";
 
-    if(!lat && !long){
-       alert("Localização desativada, ative para obter as informações meteorológicas da sua localização!")
-    }    
-      
-    const response = await restApiProvider.getWeatherForecast({
+export const useWeather = () => {
+  const [data, setdata] = useState<WeatherResponse>({} as WeatherResponse);
+  const date = new Date()
+  const hours = date.getHours() + ":" + date.getSeconds()
+
+  const loadWeather = async () => {
+    const position = await new Promise<GeolocationPosition>(
+      (resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      }
+    );
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+
+    if (!lat && !long) {
+      alert(
+        "Localização desativada, ative para obter as informações meteorológicas da sua localização!"
+      );
+    }
+
+    const response: WeatherResponse = await restApiProvider.getWeatherForecast({
       cnt: 7,
       lat: lat,
       lon: long,
@@ -27,5 +34,5 @@ export const useWeather = () => {
     loadWeather();
   }, []);
 
-  return { data };
+  return { data, hours };
 };
