8e34aa2b42353c03cb39415c5418a65840a811e7
Yan <yangabriel2012@gmail.com>
2023-09-19 23:16:31 -0300

    feat: criando interface

--- interfaces/Weather.interface.ts
+++ interfaces/Weather.interface.ts
@@ -1,6 +1,57 @@
-export interface GetWeatherForecastParams{
-  cnt: number
-  q?: string,
-  lat?: number,
-  lon?: number,
+export interface GetWeatherForecastParams {
+  cnt: number;
+  q?: string;
+  lat?: number;
+  lon?: number;
+}
+
+export interface WeatherResponse {
+  city: {
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    id: number;
+    name: string;
+    population: number;
+    sunrise: number;
+    sunset: number;
+    timezone: number;
+  };
+  cnt: number;
+  cod: string;
+  list: WeatherForecastList[];
+}
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface WeatherForecastList {
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  dt_txt: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    grnd_level: number;
+    humidity: number;
+    sea_level: number;
+    temp_kf: number;
+  };
+  weather: Weather[];
+  wind: {
+    deg: number;
+    gust: number;
+    speed: number;
+  };
 }
