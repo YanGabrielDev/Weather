3fe3ee81be39e92f02bfe4be126d2a396dbd93e0
Yan <yangabriel2012@gmail.com>
2023-09-21 23:11:32 -0300

    fix: separando a funcao se pegar a posiçao atual da request

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -4,35 +4,54 @@ import { WeatherResponse } from "../interfaces/Weather.interface";
 
 export const useWeather = () => {
   const [data, setdata] = useState<WeatherResponse>({} as WeatherResponse);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const date = new Date()
-  const hours = date.getHours() + ":" + date.getSeconds()
-
-  const loadWeather = async () => {
-    const position = await new Promise<GeolocationPosition>(
-      (resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      }
-    );
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-
-    if (!lat && !long) {
-      alert(
-        "Localização desativada, ative para obter as informações meteorológicas da sua localização!"
+  const hours = date.getHours() + ":" + date.getMinutes()
+  
+  const loadCurrentPosition = async () => {
+    try {
+      const position = await new Promise<GeolocationPosition>(
+        (resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        }
       );
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+  
+      if (!lat && !long) {
+        alert(
+          "Localização desativada, ative para obter as informações meteorológicas da sua localização!"
+        );
+        return
+      }
+      return{lat, long}
+    } catch (error) {
+      console.error("Erro ao obter a localização:", error);
+      
     }
+  }
 
-    const response: WeatherResponse = await restApiProvider.getWeatherForecast({
-      cnt: 7,
-      lat: lat,
-      lon: long,
-    });
-    setdata(response);
+  const loadWeather = async ()  => {
+    try {
+      const position = await loadCurrentPosition()
+       if(position){
+       const response: WeatherResponse = await restApiProvider.getWeatherForecast({
+         cnt: 7,
+         lat: position.lat,
+         lon: position.long,
+       });
+       setdata(response);
+       setIsLoading(false)
+     }
+    } catch (error) {
+      console.error("Erro ao obter a previsão:", error);
+      
+    }
   };
 
   useEffect(() => {
     loadWeather();
   }, []);
 
-  return { data, hours };
+  return { data, hours, isLoading };
 };
