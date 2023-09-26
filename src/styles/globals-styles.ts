19daab271fc76be0270e4ba38cc2f9309969a7db
Yan <yangabriel2012@gmail.com>
2023-09-25 21:58:38 -0300

    fix: colando o dark mode do html

--- src/styles/globals-styles.ts
+++ src/styles/globals-styles.ts
@@ -1,10 +1,12 @@
 import {createGlobalStyle} from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
- body{
+ *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+   }
+   html{
     background-color:  ${({theme}) => theme.colors.background};
  }
 `
