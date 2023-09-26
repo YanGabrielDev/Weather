c025c1a602abdd4ae34682994b90675b3bbc6126
Yan <yangabriel2012@gmail.com>
2023-09-25 21:50:23 -0300

    style: padronizando estilização do container

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -2,7 +2,9 @@ import { styled,css } from 'styled-components'
 
 export const MainContainer = styled.main`
 ${({theme}) => css`
-display: flex;
-padding: 1,25rem;
+padding: 0 1.25rem;
+width: 100%;
+margin: 0 auto;
+max-width: 1440px;
 `}
 `
