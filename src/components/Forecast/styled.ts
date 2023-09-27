5cacd7b14fb604de1a5e6150615d8d42f398ce1c
Yan <yangabriel2012@gmail.com>
2023-09-26 23:42:38 -0300

    style: corrigindo responsivo do forecast

--- src/components/Forecast/styled.ts
+++ src/components/Forecast/styled.ts
@@ -8,19 +8,27 @@ grid-template-columns: repeat(6, 1fr);
 grid-auto-rows: minmax(50px, auto);
 gap: 1rem;
 
-@media (max-width: 980px) {
+@media (max-width: 1280px) {
 grid-template-columns: repeat(3, 1fr);
 grid-auto-rows: minmax(50px, auto);
-gap: 4rem;
+gap: 1rem;
 }
 
 @media (max-width: 600px) {
 grid-template-columns: repeat(2, 1fr);
 grid-auto-rows: minmax(50px, auto);
-gap: 4rem;
+gap: 1rem;
 }
+
+@media (max-width: 415px) {
+grid-template-columns: repeat(1, 1fr);
+grid-auto-rows: minmax(50px, auto);
+gap: 1rem;
+}
+
 `}
 `
+
 export const ForecastDetails = styled.div`
 ${({theme}) =>css`
 padding: 1.25rem;
@@ -32,5 +40,18 @@ border-radius: 0.5rem;
 color: ${theme.colors.text};
 background-color: ${theme.colors.main};
 color: ${theme.colors.text};
+`}
+`
+
+export const ForecastTemp = styled.span`
+${({theme}) =>css`
+font-family: ${theme.font?.family.roboto};
+font-size: 1.25rem;
+`}
+`
+export const ForecastDay = styled.span`
+${({theme}) =>css`
+font-family: ${theme.font?.family.roboto};
+font-size: 1.5rem;
 `}
 `
