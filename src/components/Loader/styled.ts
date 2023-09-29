0e9a06a834fcd7ab136030a07785997da6c79afe
Yan <yangabriel2012@gmail.com>
2023-09-28 22:58:57 -0300

    fix: trocando animação do loader

--- src/components/Loader/styled.ts
+++ src/components/Loader/styled.ts
@@ -1,38 +1,59 @@
 import { styled, css } from "styled-components";
 
 export const Loading = styled.span`
-${({theme}) =>css`
+  ${({ theme }) => css`
     width: 48px;
     height: 48px;
-    display: inline-block;
+    display: block;
+    margin: 20px auto;
+    box-sizing: border-box;
     position: relative;
 
-  &::after,
-  &::before {
-    content: '';  
-    box-sizing: border-box;
-    width: 152px;
-    height: 152px;
-    border-radius: 50%;
-    border: 8px solid ${theme.colors.text};
-    position: absolute;
-    left: 0;
-    top: 0;
-    animation: animloader 2s linear infinite;
-  }
-  &::after {
-    animation-delay: 1s;
-  }
-  
-  @keyframes animloader {
-    0% {
-      transform: scale(0);
-      opacity: 1;
+    &::after {
+      content: "";
+      box-sizing: border-box;
+      width: 48px;
+      height: 48px;
+      left: 0;
+      bottom: 0;
+      position: absolute;
+      border-radius: 50% 50% 0;
+      border: 15px solid ${theme.colors.text};
+      transform: rotate(45deg) translate(0, 0);
+      box-sizing: border-box;
+      animation: animMarker 0.4s ease-in-out infinite alternate;
     }
-    100% {
-      transform: scale(1);
-      opacity: 0;
+    &::before {
+      content: "";
+      box-sizing: border-box;
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin: auto;
+      top: 150%;
+      width: 24px;
+      height: 4px;
+      border-radius: 50%;
+      background: rgba(0, 0, 0, 0.2);
+      animation: animShadow 0.4s ease-in-out infinite alternate;
     }
-  }
-`}
-`
+
+    @keyframes animMarker {
+      0% {
+        transform: rotate(45deg) translate(5px, 5px);
+      }
+      100% {
+        transform: rotate(45deg) translate(-5px, -5px);
+      }
+    }
+
+    @keyframes animShadow {
+      0% {
+        transform: scale(0.5);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }
+  `}
+`;
