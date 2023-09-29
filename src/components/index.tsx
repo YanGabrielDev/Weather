c7a883df35fc60a583d2a553bb7e05cc0faf3efd
Yan <yangabriel2012@gmail.com>
2023-09-28 22:46:33 -0300

    fix: adicionando os components para serem exportados

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,18 @@ import { Header } from "./Header";
 import { Layout } from "./Layout";
 import { Location } from "./Location";
 import { TextField } from "./TextField";
+import { CurrentDay } from "./CurrentDay";
+import { Forecast } from "./Forecast";
+import { Loader } from "./Loader";
 
-export { Button, Container, Header, Layout, Location, TextField };
+export {
+  Button,
+  Container,
+  Header,
+  Layout,
+  Location,
+  TextField,
+  CurrentDay,
+  Forecast,
+  Loader
+};
