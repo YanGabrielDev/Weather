906e4f8ad7e6ecd0128b7e7ad8058328fdfd9fa8
Yan <yangabriel2012@gmail.com>
2023-09-28 22:47:24 -0300

    refactor: refatorando layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,29 +3,43 @@ import { Button, Container, Header, Location, TextField } from "../";
 import { ReactNode } from "react";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import { ButtonContainer } from "./styled";
+import { useWeatherContext } from "../../hooks/useWeather";
 
-interface LayoutProps{
-    darkMode: boolean,
-    handleChangeTheme: () => void,
-    children: ReactNode
+interface LayoutProps {
+  darkMode: boolean;
+  handleChangeTheme: () => void;
+  children: ReactNode;
 }
 
-export const Layout = ({darkMode, handleChangeTheme, children}: LayoutProps) => {
-  const matchLocation = useMediaQuery('(min-width: 700px)')
+export const Layout = ({
+  darkMode,
+  handleChangeTheme,
+  children,
+}: LayoutProps) => {
+  const matchLocation = useMediaQuery("(min-width: 700px)");
+  const { loadWeatherDailyByName, data, isLoading } = useWeatherContext();
+  const { city } = data;
   return (
-    <>
-      <Container>
+    <Container>
       <Header>
-        {matchLocation && <Location city="Belo Horizonte" country="BR"/>}
-        <TextField placeholder="Procure a cidade..." />
+        {matchLocation && !isLoading && (
+          <Location city={city.name} country={city.country} />
+        )}
+        <TextField
+          placeholder="Procure a cidade..."
+          onChange={(event) => {
+            if (event.target.value.length > 3) {
+              loadWeatherDailyByName(event.target.value);
+            }
+          }}
+        />
         <ButtonContainer>
-        <Button rounded="full" color="primary" onClick={handleChangeTheme}>
-          {darkMode ? <Moon /> : <Sun />}
-        </Button>
+          <Button rounded="full" color="primary" onClick={handleChangeTheme}>
+            {darkMode ? <Moon /> : <Sun />}
+          </Button>
         </ButtonContainer>
       </Header>
-        {children}
-      </Container>
-    </>
+      {children}
+    </Container>
   );
 };
